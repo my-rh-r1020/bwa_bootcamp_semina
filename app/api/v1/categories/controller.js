84ce0afd950791d74c3fded03424c6fe5fa8c53a
my-rh-r1020 <rherikson.my@gmail.com>
2022-04-12 17:57:58 +0700

    Handle one category user

--- app/api/v1/categories/controller.js
+++ app/api/v1/categories/controller.js
@@ -17,6 +17,10 @@ const createCategory = async (req, res, next) => {
     const { name } = req.body,
       user = req.user.id;
 
+    const check = await Category.findOne({ user, name });
+
+    if (check) throw new CustomAPIError.BadRequestError("Duplicate for name category!");
+
     const result = await Category.create({ name, user });
 
     res.status(StatusCodes.CREATED).json({ data: result });
@@ -25,4 +29,18 @@ const createCategory = async (req, res, next) => {
   }
 };
 
-module.exports = { getAllCategory, createCategory };
+const getOneCategory = async (req, res, next) => {
+  try {
+    const { id: categoryId } = req.params;
+
+    const result = await User.findOne({ id: categoryId });
+
+    if (!result) throw new CustomAPIError.NotFoundError(`Data not found for id ${categoryId}`);
+
+    res.status(StatusCodes.OK).json({ data: result });
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports = { getAllCategory, createCategory, getOneCategory };
