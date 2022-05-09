c7c65afba7fd4af3fe014eac0ee9ef5b22fdb71c
my-rh-r1020 <rherikson.my@gmail.com>
2022-05-09 14:01:00 +0700

    Session 7 - Handle Payment method

--- app/api/v1/payments/controller.js
+++ app/api/v1/payments/controller.js
@@ -1,5 +1,119 @@
 const Payment = require("./model"),
   { StatusCodes } = require("http-status-codes"),
-  CustomAPIError = require("../../../errors");
+  CustomAPIError = require("../../../errors"),
+  fs = require("fs"),
+  config = require("../../../config");
 
-module.exports = {};
+// Get all data payments
+const getAllPayments = async (req, res, next) => {
+  try {
+    const { status } = req.query;
+
+    let condition = { user: req.user.id };
+
+    if (status) {
+      condition = { ...condition, status: status };
+    }
+
+    const result = await Payment.find(condition);
+
+    res.status(StatusCodes.OK).json({ data: result });
+  } catch (err) {
+    next(err);
+  }
+};
+
+// Get a data payment by id
+const getOnePayment = async (req, res, next) => {
+  try {
+    const { id: PaymentId } = req.params;
+
+    const result = await Payment.findOne({ _id: PaymentId, user: req.user.id });
+
+    if (!result) throw new CustomAPIError.NotFoundError(`Payment id ${PaymentId} is not found!`);
+
+    res.status(StatusCodes.OK).json({ data: result });
+  } catch (err) {
+    next(err);
+  }
+};
+
+// Create a new data payment
+const createPayment = async (req, res, next) => {
+  try {
+    const { type, status } = req.body,
+      user = req.user.id;
+
+    let result;
+
+    if (!req.file) {
+      result = await Payment({ type, status, user }).save();
+    } else {
+      result = await Payment({ type, imageUrl: req.file.filename, status, user }).save();
+    }
+
+    res.status(StatusCodes.CREATED).json({ data: result });
+  } catch (err) {
+    next(err);
+  }
+};
+
+// Update a data payment by id
+const updatePayment = async (req, res, next) => {
+  try {
+    const { type, status } = req.body,
+      { id: PaymentId } = req.params,
+      user = req.user.id;
+
+    let result = await Payment.findOne({ _id: PaymentId, user });
+
+    if (!result) throw new CustomAPIError.NotFoundError(`Payment id ${PaymentId} is not found!`);
+
+    if (!req.file) {
+      result.type = type;
+      result.status = status;
+    } else {
+      let currentImage = `${config.rootPath}/public/uploads/payments/${result.imageUrl}`;
+
+      if (result.imageUrl !== "default.png" && fs.existsSync(currentImage)) {
+        fs.unlinkSync(currentImage);
+      }
+
+      result.type = type;
+      result.status = status;
+      result.imageUrl = req.file.filename;
+    }
+
+    await result.save();
+
+    res.status(StatusCodes.OK).json({ data: result });
+  } catch (err) {
+    next(err);
+  }
+};
+
+// Delete a data payment by id
+const deletePayment = async (req, res, next) => {
+  try {
+    const { id: SpeakerId } = req.params,
+      user = req.user.id;
+
+    let result = await Speaker.findOne({ _id: SpeakerId, user });
+
+    if (!result) throw new CustomAPIError.NotFoundError(`Speaker id ${SpeakerId} is not found!`);
+
+    let currentImage = `${config.rootPath}/public/uploads/${result.avatar}`;
+
+    if (result.avatar !== "default.png" && fs.existsSync(currentImage)) {
+      fs.unlinkSync(currentImage);
+    }
+
+    await result.remove();
+
+    res.status(StatusCodes.OK).json({ data: result });
+  } catch (err) {
+    next(er);
+  }
+};
+
+module.exports = { getAllPayments, getOnePayment, createPayment, updatePayment, deletePayment };
--- app/api/v1/payments/router.js
+++ app/api/v1/payments/router.js
@@ -1,5 +1,11 @@
 const router = require("express").Router(),
-  {} = require("./controller"),
+  { getAllPayments, getOnePayment, createPayment, updatePayment, deletePayment } = require("./controller"),
   { authenticateUser } = require("../../../middlewares/auth");
+
+router.get("/", authenticateUser, getAllPayments);
+router.get("/:id", authenticateUser, getOnePayment);
+router.post("/", authenticateUser, createPayment);
+router.put("/:id", authenticateUser, updatePayment);
+router.delete("/:id", authenticateUser, deletePayment);
 
 module.exports = router;
