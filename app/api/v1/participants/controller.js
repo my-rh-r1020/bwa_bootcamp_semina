6685b6f7c1e6d36ebe757f762dbe8bbc6a38c8f1
my-rh-r1020 <rherikson.my@gmail.com>
2022-05-10 10:50:12 +0700

    Session 7 - Handle signup participants is finished

--- app/api/v1/participants/controller.js
+++ app/api/v1/participants/controller.js
@@ -6,21 +6,14 @@ const Participant = require("./model"),
 // Signup Participant
 const signup = async (req, res, next) => {
   try {
-    const { email, firstname, lastName, password, role } = req.body,
-      result = new Participant({ email, firstname, lastName, password, role });
+    const { email, firstName, lastName, password, role } = req.body,
+      result = new Participant({ email, firstName, lastName, password, role });
 
     await result.save();
 
     delete result._doc.password;
 
     res.status(StatusCodes.CREATED).json({ data: result });
-  } catch (err) {
-    next(err);
-  }
-};
-
-const getAllParticipants = async (req, res, next) => {
-  try {
   } catch (err) {
     next(err);
   }
