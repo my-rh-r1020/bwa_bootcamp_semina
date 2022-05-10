621e9cd204aa3e53d5b7a3b0e5751e34eea8fa4d
my-rh-r1020 <rherikson.my@gmail.com>
2022-05-10 13:59:40 +0700

    Session 7 - Add mock for transactions

--- app/api/v1/transactions/model.js
+++ app/api/v1/transactions/model.js
@@ -2,54 +2,97 @@ const mongoose = require("mongoose");
 
 const TransactionSchema = new mongoose.Schema(
   {
-    title: {
-      type: String,
-      required: [true, "Please provide title!"],
-      minlength: 3,
-      maxlength: 50,
+    personalDetail: {
+      firstName: {
+        type: String,
+        required: [true, "First name is required"],
+        minlength: 3,
+        maxlength: 50,
+      },
+      lastName: {
+        type: String,
+        required: [true, "Last name is required"],
+        minlength: 3,
+        maxlength: 50,
+      },
+      email: {
+        type: String,
+        required: [true, "Email is required"],
+      },
+      role: {
+        type: String,
+        default: "Designer",
+      },
     },
-    price: {
-      type: Number,
-      default: 0,
-    },
-    date: {
-      type: Date,
-      required: [true, "Please provide date!"],
-    },
-    cover: {
-      type: String,
-      required: [true, "Please provide cover!"],
-    },
-    about: {
-      type: String,
-      required: [true, "Please provide about!"],
-    },
-    venueName: {
-      type: String,
-      required: [true, "Please provide venue name!"],
-    },
-    tagline: {
-      type: String,
-      required: [true, "Please provide tagline!"],
-    },
-    keypoint: {
-      type: [String],
-      // required: [true, "Please provide keypoint!"],
-    },
-    category: {
+    participant: {
       type: mongoose.Types.ObjectId,
-      ref: "Category",
+      ref: "Participant",
       required: true,
     },
-    speaker: {
+    event: {
       type: mongoose.Types.ObjectId,
-      ref: "Speaker",
+      ref: "Event",
       required: true,
     },
-    user: {
+    historyEvent: {
+      title: {
+        type: String,
+        required: [true, "Please provide title!"],
+      },
+      price: {
+        type: Number,
+        default: 0,
+      },
+      date: {
+        type: Date,
+        required: [true, "Please provide date!"],
+      },
+      cover: {
+        type: String,
+        required: [true, "Please provide cover!"],
+      },
+      about: {
+        type: String,
+        required: [true, "Please provide about!"],
+      },
+      venueName: {
+        type: String,
+        required: [true, "Please provide venue name!"],
+      },
+      tagline: {
+        type: String,
+        required: [true, "Please provide tagline!"],
+      },
+      keypoint: {
+        type: [String],
+      },
+      category: {
+        type: mongoose.Types.ObjectId,
+        ref: "Category",
+        required: true,
+      },
+      speaker: {
+        type: mongoose.Types.ObjectId,
+        ref: "Speaker",
+        required: true,
+      },
+    },
+    payment: {
       type: mongoose.Types.ObjectId,
-      ref: "User",
+      ref: "Payment",
       required: true,
+    },
+    historyPayment: {
+      type: {
+        type: String,
+        required: [true, "Please provide type"],
+        minlength: 3,
+        maxlength: 50,
+      },
+      imageUrl: {
+        type: String,
+        required: [true, "Please provide imageUrl"],
+      },
     },
   },
   { timestamps: true }
