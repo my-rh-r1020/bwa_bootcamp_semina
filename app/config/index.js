edc152c9cf8c027ad4a355bd2cad8a5b6b07b718
my-rh-r1020 <rherikson.my@gmail.com>
2022-07-05 18:02:38 +0700

    Session 16 - Setup for upload to clouds

--- app/config/index.js
+++ app/config/index.js
@@ -1,4 +1,4 @@
 const dotenv = require("dotenv").config(),
   path = require("path");
 
-module.exports = { rootPath: path.resolve(__dirname, "../../"), urlDb: process.env.MONGODB_URL_DEV, jwtSecret: process.env.JWT_SECRET };
+module.exports = { rootPath: path.resolve(__dirname, "../../"), urlDb: process.env.MODE === "PRO" ? process.env.MONGODB_URL_PRO : process.env.MONGODB_URL_DEV, jwtSecret: process.env.JWT_SECRET };
